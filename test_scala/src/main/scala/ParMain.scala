9b76f783c5bdb2386f04d66a17e9cbbd5bd5cdcd
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2018-08-07 16:11:59 +0300

    Has been decreased limit for measuring loop

--- test_scala/src/main/scala/ParMain.scala
+++ test_scala/src/main/scala/ParMain.scala
@@ -1,6 +1,7 @@
 object ParMain extends App {
   //println("Hello, World!")
-  val toval = 2147483647L - 1L
+  //val toval = 2147483647L - 1L
+  val toval: Long = Int.MaxValue / 100
   //var summa = BigInt(0)
   val summa = (1L to toval).par.fold(0L)(_+_)
   println(s"Summa from 1 to ${toval} = ${ summa }")
