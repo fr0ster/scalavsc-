aa7a78bf69906f24a34b243729160eb09a9c6d48
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2018-08-07 15:03:17 +0300

    Has been added scala object for Paralleling Data Collection and build.sbt

---
+++ test_scala/src/main/scala/ParMain.scala
@@ -0,0 +1,7 @@
+object ParMain extends App {
+  //println("Hello, World!")
+  val toval = 2147483647L - 1L
+  //var summa = BigInt(0)
+  val summa = (1L to toval).par.fold(0L)(_+_)
+  println(s"Summa from 1 to ${toval} = ${ summa }")
+}
