9ab14103b2960f99a1ee6d25f9eebd0d6d905532
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2018-08-08 15:03:39 +0300

    Has been modified reult output in ParTest

--- test_scala/src/main/scala/ParTest.scala
+++ test_scala/src/main/scala/ParTest.scala
@@ -5,6 +5,6 @@ object ParTest extends App {
   val r = new scala.util.Random(100)
   val xs = for (i <- 1 to 500000) yield r.nextInt(100)
   val xs_par = xs.par
-  println(s"time execution long loop == ${Vector.range(0,1000).map(x=>Timed(xs.filter(_%3==0))._1).fold(0.0){_+_}/1000}")
-  println(s"result execution long loop == ${Vector.range(0,1000).map(x=>Timed(xs_par.filter(_%3==0))._1).fold(0.0){_+_}/1000}")
+  println(s"time execution Map for Vector == ${Vector.range(0,1000).map(x=>Timed(xs.filter(_%3==0))._1).fold(0.0){_+_}/1000}")
+  println(s"result execution Map for ParVector == ${Vector.range(0,1000).map(x=>Timed(xs_par.filter(_%3==0))._1).fold(0.0){_+_}/1000}")
 }
