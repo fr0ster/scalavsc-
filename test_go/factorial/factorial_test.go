4ed2c1607aed94b3a8a2053ad517d1131f02cb0c
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2022-04-09 20:11:08 +0300

    Add test for factorial with 100 as parameter

--- test_go/factorial/factorial_test.go
+++ test_go/factorial/factorial_test.go
@@ -10,12 +10,13 @@ func benchmarkFac(n int64, b *testing.B) {
 	}
 }
 
-func BenchmarkFac1(b *testing.B)  { benchmarkFac(1, b) }
-func BenchmarkFac2(b *testing.B)  { benchmarkFac(2, b) }
-func BenchmarkFac3(b *testing.B)  { benchmarkFac(3, b) }
-func BenchmarkFac10(b *testing.B) { benchmarkFac(10, b) }
-func BenchmarkFac20(b *testing.B) { benchmarkFac(20, b) }
-func BenchmarkFac40(b *testing.B) { benchmarkFac(40, b) }
+func BenchmarkFac1(b *testing.B)   { benchmarkFac(1, b) }
+func BenchmarkFac2(b *testing.B)   { benchmarkFac(2, b) }
+func BenchmarkFac3(b *testing.B)   { benchmarkFac(3, b) }
+func BenchmarkFac10(b *testing.B)  { benchmarkFac(10, b) }
+func BenchmarkFac20(b *testing.B)  { benchmarkFac(20, b) }
+func BenchmarkFac40(b *testing.B)  { benchmarkFac(40, b) }
+func BenchmarkFac100(b *testing.B) { benchmarkFac(100, b) }
 
 func TestFac(t *testing.T) {
 	if Fac(5).Int64() != 120 {
