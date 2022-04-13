39a313e83e3bf3945c94290a61c6f1b51486169e
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2022-04-13 12:48:50 +0300

    test_rust: Add modules into main.rs

--- test_rust/src/main.rs
+++ test_rust/src/main.rs
@@ -1,17 +1,15 @@
-// use std::ops::Add;
-use std::ops::*;
-
 fn main() {
     println!("Hello, world!");
-    println!("{}", foo(20,22));
+    println!("{}", foo::foo(20, 22));
 }
 
-fn foo<T>(a: T, b: T) -> T 
-where T: Add<Output=T> + Copy + Clone + Div<Output=T> + Mul<Output=T>
-{
-    return a + b;
-}
+mod foo {
+    use std::ops::*;
 
-// fn foo<T:Add>(x : T) -> T{
-//     return x + x;
-// }
+    pub fn foo<T>(a: T, b: T) -> T
+    where
+        T: Add<Output = T> + Copy + Clone + Div<Output = T> + Mul<Output = T>,
+    {
+        return a + b;
+    }
+}
